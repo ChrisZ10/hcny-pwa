380f4c1e26198bd4eeb615e0d8aaf25e659db831
Chris Zhao <chrislinzhao@gmail.com>
2021-06-09 14:21:58 -0400

    built navbar added toggle dropdown navbar

--- src/components/header.js
+++ src/components/header.js
@@ -7,6 +7,7 @@ import * as headerStyles from '../styles/modules/header.module.scss';
 const Header = () => {
   const [ active, setActive ] = useState(false);
   const [ toggle, setToggle ] = useState(false);
+  const [ drop, setDrop ] = useState(false);
 
   return (
     <header className = { headerStyles.header }>
@@ -22,11 +23,63 @@ const Header = () => {
         </div>
         
         <ul className = { active? headerStyles.navbarActive : headerStyles.navbar }>
-          <li>
+          
+          {/******* New Friend *******/}
+          <li 
+            onMouseEnter = {() => {
+              setDrop(true);
+            }}
+            onMouseLeave = {() => {
+              setDrop(false);
+            }}
+          >
             <Link to = "#" className = { headerStyles.navbarItem }>新朋友？</Link>
+            <ul className = { drop? headerStyles.navbarDropdownActive : headerStyles.navbarDropdown }>
+              <li>
+                <Link to = "#" className = { headerStyles.navbarDropdownItem }>關於我們</Link>
+              </li>
+              <li>
+                <Link to = "#" className = { headerStyles.navbarDropdownItem }>聚會時間</Link>
+              </li>
+              <li>
+                <Link to = "#" className = { headerStyles.navbarDropdownItem }>牧者團隊</Link>
+              </li>
+              <li>
+                <Link to = "#" className = { headerStyles.navbarDropdownItem }>尋求代禱</Link>
+              </li>
+              <li>
+                <Link to = "#" className = { headerStyles.navbarDropdownItem }>聯繫我們</Link>
+              </li>
+            </ul>
           </li>
-          <li>
+          
+          {/******* Get Involved *******/}
+          <li
+            onMouseEnter = {() => {
+              setDrop(true);
+            }}
+            onMouseLeave = {() => {
+              setDrop(false);
+            }}
+          >
             <Link to = "#" className = { headerStyles.navbarItem }>豐收大家庭</Link>
+            <ul className = { drop? headerStyles.navbarDropdownActive : headerStyles.navbarDropdown }>
+              <li>
+                <Link to = "#" className = { headerStyles.navbarDropdownItem }>細胞小組</Link>
+              </li>
+              <li>
+                <Link to = "#" className = { headerStyles.navbarDropdownItem }>豐收義工團隊</Link>
+              </li>
+              <li>
+                <Link to = "#" className = { headerStyles.navbarDropdownItem }>通知看板</Link>
+              </li>
+              <li>
+                <Link to = "#" className = { headerStyles.navbarDropdownItem }>近期活動</Link>
+              </li>
+              <li>
+                <Link to = "#" className = { headerStyles.navbarDropdownItem }>代禱事項</Link>
+              </li>
+            </ul>
           </li>
           <li>
             <Link to = "#" className = { headerStyles.navbarItem }>靈修裝備</Link>
