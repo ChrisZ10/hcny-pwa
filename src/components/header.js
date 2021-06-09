914ec222abdc70099976c967b06355efbcf64fd3
Chris Zhao <chrislinzhao@gmail.com>
2021-06-09 11:58:28 -0400

    built navbar added toggle animation added burger transform animation

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'gatsby';
 import { StaticImage } from 'gatsby-plugin-image';
 
 import * as headerStyles from '../styles/modules/header.module.scss';
 
 const Header = () => {
+  const [ active, setActive ] = useState(false);
+  const [ toggle, setToggle ] = useState(false);
+
   return (
     <header className = { headerStyles.header }>
       <nav>
@@ -18,7 +21,7 @@ const Header = () => {
           />  
         </div>
         
-        <ul className = { headerStyles.navbar }>
+        <ul className = { active? headerStyles.navbarActive : headerStyles.navbar }>
           <li>
             <Link to = "#" className = { headerStyles.navbarItem }>新朋友？</Link>
           </li>
@@ -33,7 +36,13 @@ const Header = () => {
           </li>
         </ul>
         
-        <div className = { headerStyles.burger }>
+        <div 
+          className = { toggle? headerStyles.burgerToggle : headerStyles.burger } 
+          onClick = {() => {
+            setActive( !active );
+            setToggle( !toggle );
+          }}
+        >
           <div className = { headerStyles.line1 }></div>
           <div className = { headerStyles.line2 }></div>
           <div className = { headerStyles.line3 }></div>
