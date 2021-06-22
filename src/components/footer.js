dc97379a8a21118d92dc7a2204f42fd0ad89eef9
Chris Zhao <chrislinzhao@gmail.com>
2021-06-22 12:09:07 -0400

    built footer added advanced styles added interaction

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
-import { FaYoutube, FaFacebookSquare, FaCopyright } from 'react-icons/fa';
+import { graphql, useStaticQuery, Link } from 'gatsby';
+import { FaYoutube, FaFacebookSquare } from 'react-icons/fa';
 
 import * as footerStyles from '../styles/modules/footer.module.scss';
 
@@ -20,16 +20,24 @@ const Footer = () => {
       <p className = { footerStyles.slogan }>{ data.site.siteMetadata.slogan }</p>
       <p className = { footerStyles.title }>{ data.site.siteMetadata.title }</p>
       <p className = { footerStyles.smContainer }>
-        <FaYoutube className = { footerStyles.youtube } />
-        <FaFacebookSquare className = { footerStyles.facebook } />
+        <Link to = "#youtube">
+          <FaYoutube className = { footerStyles.youtube } />
+        </Link> 
+        <Link to = "#facebook">
+          <FaFacebookSquare className = { footerStyles.facebook } />
+        </Link>
       </p>
       <p className = { footerStyles.address }>{ data.site.siteMetadata.address }</p>
       <p className = { footerStyles.email }>{ data.site.siteMetadata.email }</p>
       <p className = { footerStyles.phone }>{ data.site.siteMetadata.phone }</p>
       <p className = { footerStyles.copyright }>&copy; 2021 Harvest Church of New York. All Rights Reserved</p>
       <p className = { footerStyles.legal }>
-        <span>Terms of Use</span>
-        <span>Privacy Policy</span>
+        <Link to = "#termsOfUse" className = {footerStyles.legalLink}>
+          <span>Terms of Use</span>
+        </Link>
+        <Link to = "#privacyPolicy" className = {footerStyles.legalLink}>
+          <span>Privacy Policy</span>
+        </Link>
       </p>
     </footer>
   );
