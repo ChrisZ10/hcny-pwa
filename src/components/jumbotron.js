e7a000c6cbbdfa7872fcea534821abeae777746e
Chris Zhao <chrislinzhao@gmail.com>
2021-06-23 15:22:47 -0400

    built jumbotron decorated jumbotron added chevron animation added scrolldown logic

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import { BsChevronDown } from 'react-icons/bs';
 
 import * as jumbotronStyles from '../styles/modules/jumbotron.module.scss';
 
 const Jumbotron = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }    
+  `);
+
+  const [scrollTop, setScrollTop] = useState(0);
+  const [scrolling, setScrolling] = useState(true);
+
+  useEffect(() => {
+    const onScroll = ( event ) => {
+      setScrollTop(event.target.documentElement.scrollTop);
+      setScrolling(event.target.documentElement.scrollTop < scrollTop);
+    };
+    window.addEventListener("scroll", onScroll);
+  }, [scrollTop]);
+
   return (
-    <div className = { jumbotronStyles.container }>Jumbotron</div>
+    <div className = { jumbotronStyles.container }>
+      <div className = { jumbotronStyles.main }>
+        <h1 className = { jumbotronStyles.subtitle }>歡迎來到</h1>
+        <h1 className = { jumbotronStyles.title }>{ data.site.siteMetadata.title }</h1>
+        <div className = { jumbotronStyles.buttonContainer }>
+          <a href = "#sunday-stream" className = { jumbotronStyles.button }>觀看主日直播</a>
+        </div>
+      </div>
+      <BsChevronDown 
+        className = { scrolling? jumbotronStyles.chevronActive : jumbotronStyles.chevron }
+      />
+    </div>
   );
 }
 
