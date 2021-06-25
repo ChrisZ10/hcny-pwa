5f9ac30f2b260031f20b5fe6883290205d17da81
Chris Zhao <chrislinzhao@gmail.com>
2021-06-25 13:54:31 -0400

    built jumbotron added documentation

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -15,9 +15,10 @@ const Jumbotron = () => {
     }    
   `);
 
-  const [scrollTop, setScrollTop] = useState(0);
-  const [scrolling, setScrolling] = useState(true);
+  const [scrollTop, setScrollTop] = useState(0); // number of pixels the root element is scrolled
+  const [scrolling, setScrolling] = useState(true); // set scrolling to be true when scrolling down
 
+  // update states every time the root element is scrolled 
   useEffect(() => {
     const onScroll = ( event ) => {
       setScrollTop(event.target.documentElement.scrollTop);
@@ -31,13 +32,12 @@ const Jumbotron = () => {
       <div className = { jumbotronStyles.main }>
         <h1 className = { jumbotronStyles.subtitle }>歡迎來到</h1>
         <h1 className = { jumbotronStyles.title }>{ data.site.siteMetadata.title }</h1>
+        
         <div className = { jumbotronStyles.buttonContainer }>
           <a href = "#sunday-stream" className = { jumbotronStyles.button }>觀看主日直播</a>
         </div>
       </div>
-      <BsChevronDown 
-        className = { scrolling? jumbotronStyles.chevronActive : jumbotronStyles.chevron }
-      />
+      <BsChevronDown className = { scrolling? jumbotronStyles.chevronActive : jumbotronStyles.chevron }/>
     </div>
   );
 }
