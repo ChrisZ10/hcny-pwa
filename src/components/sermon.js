9a98694986d40759805cb3b22e284458c56b4c07
Chris Zhao <chrislinzhao@gmail.com>
2021-06-28 13:24:16 -0400

    built sermon section enabled access to sermon cover image on wp via graphql

--- src/components/sermon.js
+++ src/components/sermon.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import * as sermonStyles from '../styles/modules/sermon.module.scss';
 
 const Sermon = () => {
-    return (
-        <div className = { sermonStyles.container }>
-            <div className = { sermonStyles.leftColumn }>Left Column</div>
-            <div className = { sermonStyles.rightColumn }>Right Column</div>
-        </div>
-    );
+  const data = useStaticQuery(graphql`
+    query {
+      wpContent {
+        mediaItems(where: {title: "sermon-cover"}) {
+          edges {
+            node {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+  console.log(data.wpContent.mediaItems.edges[0].node.slug);
+
+  return (
+    <div className = { sermonStyles.container }>
+      <div className = { sermonStyles.leftColumn }>Left Column</div>
+      <div className = { sermonStyles.rightColumn }>Right Column</div>
+    </div>
+  );
 }
 
 export default Sermon;
