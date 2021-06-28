62d3d8e9a63708acbd5283978acd3ffa08a837ec
Chris Zhao <chrislinzhao@gmail.com>
2021-06-28 14:36:14 -0400

    built sermon section built cover

--- src/components/sermon.js
+++ src/components/sermon.js
@@ -1,27 +1,21 @@
 import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 
 import * as sermonStyles from '../styles/modules/sermon.module.scss';
 
 const Sermon = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      wpContent {
-        mediaItems(where: {title: "sermon-cover"}) {
-          edges {
-            node {
-              slug
-            }
-          }
-        }
-      }
-    }
-  `);
-  console.log(data.wpContent.mediaItems.edges[0].node.slug);
 
   return (
     <div className = { sermonStyles.container }>
-      <div className = { sermonStyles.leftColumn }>Left Column</div>
+      <div className = { sermonStyles.leftColumn }>
+        <StaticImage
+          src = "../assets/sermon_cover.jpg"
+          alt = "sermon cover"
+          placeholder = "blurred"
+          layout = "constrained"
+          className = { sermonStyles.cover }
+        />
+      </div>
       <div className = { sermonStyles.rightColumn }>Right Column</div>
     </div>
   );
