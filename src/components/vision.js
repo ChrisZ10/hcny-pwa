d208bdb1cd2575e152c9403c7a8448156f264a62
Chris Zhao <chrislinzhao@gmail.com>
2021-06-29 14:47:48 -0400

    built vision section added styles added interaction styles

--- src/components/vision.js
+++ src/components/vision.js
@@ -3,13 +3,20 @@ import React from 'react';
 import * as visionStyles from '../styles/modules/vision.module.scss';
 
 const Vision = () => {
+  const data = {
+    subtitle: "豐收靈糧堂異象",
+    title: "建造榮耀的教會",
+    description: "We shall follow the vision as revealed in Revelation Chapter 22 to build the glorious church of Jesus Christ"
+  }
+
   return (
     <div className = { visionStyles.container }>
-      <p>豐收靈糧堂異象</p>
-      <p>建造榮耀的教會</p>
-      <p>We shall follow the vision as revealed in Revelation Chapter 22 to build the glorious church of Jesus Christ</p>
-      <div>
-        <a>了解教會異象</a>
+      <p className = { visionStyles.subtitle }>{ data.subtitle }</p>
+      <p className = { visionStyles.title }>{ data.title }</p>
+      <p className = { visionStyles.description }>{ data.description }</p>
+
+      <div className = { visionStyles.buttonContainer }>
+        <a href = "#vision" className = { visionStyles.button } >了解教會異象</a>
       </div>
     </div>
   );
