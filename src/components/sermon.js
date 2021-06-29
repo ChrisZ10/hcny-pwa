4b82a5185153bd7a50066fb5d6eb0e1a8758442c
Chris Zhao <chrislinzhao@gmail.com>
2021-06-29 14:28:33 -0400

    built sermon section added documentation modified styles modified header and jumbotron styles

--- src/components/sermon.js
+++ src/components/sermon.js
@@ -5,8 +5,17 @@ import * as sermonStyles from '../styles/modules/sermon.module.scss';
 
 const Sermon = () => {
 
+  const data = {
+    date: "6月27日",
+    title: "呼召吹號者",
+    speaker: "鄭立新 牧師"
+  }
+
   return (
+    /******* 2-column grid (7:5) *******/
     <div className = { sermonStyles.container }>
+      
+      {/******* left column *******/}
       <div className = { sermonStyles.leftColumn }>
         <StaticImage
           src = "../assets/sermon_cover.jpg"
@@ -16,15 +25,19 @@ const Sermon = () => {
           className = { sermonStyles.cover }
         />
       </div>
-      <div className = { sermonStyles.rightColumn }>
-        <p className = { sermonStyles.subtitle }>主日信息 | 6月27日</p>
-        <p className = { sermonStyles.title }>呼召吹號者</p>
-        <p className = { sermonStyles.speaker }>鄭立新 牧師</p>
+      
+      {/******* right column *******/}
+      <div className = { sermonStyles.rightColumn }>       
+        <p className = { sermonStyles.subtitle }>主日信息 | { data.date }</p>
+        <p className = { sermonStyles.title }>{ data.title }</p>
+        <p className = { sermonStyles.speaker }>{ data.speaker }</p>
+
         <div className = { sermonStyles.buttonContainer }>
           <a href = "#watch" className = { sermonStyles.button }>觀看</a>
           <a href = "#more" className = { sermonStyles.buttonHollow }>更多</a>
         </div>
       </div>
+    
     </div>
   );
 }
