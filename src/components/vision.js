17155acbe9993b81e845b57e572fd5812dfafe99
Chris Zhao <chrislinzhao@gmail.com>
2021-07-06 13:09:15 -0400

    built vision section use graphql to fetch metadata add documentation

--- src/components/vision.js
+++ src/components/vision.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import * as visionStyles from '../styles/modules/vision.module.scss';
 
 const Vision = () => {
+  
+  /******* Fetch slogan from site metadata *******/
+  const res = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          slogan
+        }
+      }
+    }    
+  `);
+
   const data = {
     subtitle: "豐收靈糧堂異象",
-    title: "建造榮耀的教會",
+    title: res.site.siteMetadata.slogan,
     description: "We shall follow the vision as revealed in Revelation Chapter 22 to build the glorious church of Jesus Christ"
   }
 
