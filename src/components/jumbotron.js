61487b614935e40a2c9d36f20e59b8d07b6267c9
Chris Zhao <chrislinzhao@gmail.com>
2021-07-07 13:44:32 -0400

    use gatsby-plugin-image for jumbotron background image

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
+import { StaticImage } from 'gatsby-plugin-image';
 import { BsChevronDown } from 'react-icons/bs';
 
 import * as jumbotronStyles from '../styles/modules/jumbotron.module.scss';
@@ -28,16 +29,26 @@ const Jumbotron = () => {
   }, [scrollTop]);
 
   return (
-    <div className = { jumbotronStyles.container }>
-      <div className = { jumbotronStyles.main }>
-        <h1 className = { jumbotronStyles.subtitle }>歡迎來到</h1>
-        <h1 className = { jumbotronStyles.title }>{ data.site.siteMetadata.title }</h1>
-        
-        <div className = { jumbotronStyles.buttonContainer }>
-          <a href = "#sunday-stream" className = { jumbotronStyles.button }>觀看主日直播</a>
+    <div className = { jumbotronStyles.grid }>      
+      <StaticImage
+        className = { jumbotronStyles.background }
+        src = "../assets/wheat.jpg"
+        alt = "wheat"
+        layout = "fullWidth"
+        placeholder = "blurred"
+      />
+
+      <div className = { jumbotronStyles.container }>
+        <div className = { jumbotronStyles.main }>
+          <h1 className = { jumbotronStyles.subtitle }>歡迎來到</h1>
+          <h1 className = { jumbotronStyles.title }>{ data.site.siteMetadata.title }</h1>
+          
+          <div className = { jumbotronStyles.buttonContainer }>
+            <a href = "#sunday-stream" className = { jumbotronStyles.button }>觀看主日直播</a>
+          </div>
         </div>
+        <BsChevronDown className = { scrolling? jumbotronStyles.chevronActive : jumbotronStyles.chevron }/>
       </div>
-      <BsChevronDown className = { scrolling? jumbotronStyles.chevronActive : jumbotronStyles.chevron }/>
     </div>
   );
 }
