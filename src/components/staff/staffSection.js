3b533f9d8d23d500e9831dd2101a37ad41c4d107
Chris Zhao <chrislinzhao@gmail.com>
2021-07-21 15:36:49 -0400

    built staff page built staff section

---
+++ src/components/staff/staffSection.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import * as staffSectionStyles from '../../styles/modules/staff/staffSection.module.scss';
+
+const StaffSection = ({ title, children }) => {
+  return (
+    <div className = { staffSectionStyles.container }>
+      <h1>{ title }</h1>
+      <div>
+        { children }
+      </div>
+    </div>
+  );
+};
+
+export default StaffSection;
--- src/pages/staff.js
+++ src/pages/staff.js
@@ -4,6 +4,7 @@ import { useStaticQuery, graphql } from 'gatsby';
 import Layout from '../components/layout';
 import Head from '../components/head';
 import Banner from '../components/banner';
+import StaffSection from '../components/staff/staffSection';
 
 const Staff = () => {
   
@@ -35,6 +36,23 @@ const Staff = () => {
         imageData = { data.banner } 
         title = "教會牧者和同工團隊"
       />
+      <StaffSection title = "教牧團">
+        <div><h3>Pastor-1</h3></div>
+        <div><h3>Pastor-2</h3></div>
+        <div><h3>Pastor-3</h3></div>
+      </StaffSection>
+      <StaffSection title = "理事會">
+        <div><h3>Board-1</h3></div>
+        <div><h3>Board-2</h3></div>
+        <div><h3>Board-3</h3></div>
+        <div><h3>Board-4</h3></div>
+      </StaffSection>
+      <StaffSection title = "其它主要同工">
+        <div><h3>Staff-1</h3></div>
+        <div><h3>Staff-2</h3></div>
+        <div><h3>Staff-3</h3></div>
+        <div><h3>Staff-4</h3></div>
+      </StaffSection>
     </Layout>
   );
 };
