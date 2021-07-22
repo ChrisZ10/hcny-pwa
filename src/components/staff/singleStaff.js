4fbc8bf9c922b25121c409c3baa236c0d23524cc
Chris Zhao <chrislinzhao@gmail.com>
2021-07-22 14:52:12 -0400

    built staff page edit styles of staff page

--- src/components/staff/singleStaff.js
+++ src/components/staff/singleStaff.js
@@ -6,20 +6,25 @@ import * as singleStaffStyles from '../../styles/modules/staff/singleStaff.modul
 const SingleStaff = ({ name, title, gender }) => {
   return (
     <div className = { singleStaffStyles.container }>
+      
+      {/******* display default avatar according to gender, later should be replaced by real photos *******/}
       {gender === "m"? 
       <StaticImage
-        src = "../../assets/male.png"
+        src = "../../assets/male.jpg"
         alt = "male avatar"
         placeholder = "blurred"
         layout = "constrained"
+        className = { singleStaffStyles.image}
       /> :
       <StaticImage
-        src = "../../assets/female.png"
+        src = "../../assets/female.jpg"
         alt = "female avatar"
         placeholder = "blurred"
         layout = "constrained"
+        className = { singleStaffStyles.image}
       />
       }
+      
       <h1 className = { singleStaffStyles.title }>{ name }</h1>
       <h1 className = { singleStaffStyles.subtitle }>{ title }</h1>
     </div>
