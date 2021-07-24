09b32fd6c1561966c9eb11f9cd5a96987d657b6e
Chris Zhao <chrislinzhao@gmail.com>
2021-07-24 15:02:38 -0400

    added basic components to contact-us page

--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+import Layout from '../components/layout';
+import Head from '../components/head';
+
 const ContactUs = () => {
   return (
-    <div>Contact Us</div>
+    <Layout>
+      <Head subtitle = "联系我們"/>
+    </Layout>
   );
 };
 
