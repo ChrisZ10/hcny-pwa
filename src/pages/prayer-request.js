a14ae86f735111b5c3cd99943ad830f0996d706c
Chris Zhao <chrislinzhao@gmail.com>
2021-07-28 14:14:39 -0400

    created prayer-request page added banner to prayer-request page

---
+++ src/pages/prayer-request.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+
+import Layout from '../components/layout';
+import Head from '../components/head';
+import Banner from '../components/banner';
+
+const PrayerRequest = () => {
+
+  const data = useStaticQuery(graphql`
+    query {      
+      banner: imageSharp (
+        original: {
+          src: {
+            regex: "/.*pray.*/"
+          }
+        }
+      ) {
+        gatsbyImageData (
+          placeholder: BLURRED,
+          aspectRatio: 2.5,
+          layout: FULL_WIDTH,
+          transformOptions: {
+            fit: OUTSIDE
+          }
+        )
+      }
+    }
+  `);
+
+  return (
+    <Layout>
+      <Head subtitle = "尋求代禱" />
+      <Banner 
+        imageData = { data.banner } 
+        title = "尋求代禱"
+      />
+    </Layout>
+  );
+};
+
+export default PrayerRequest;
